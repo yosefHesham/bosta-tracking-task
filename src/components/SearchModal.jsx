528d0822288b52874f9e0e6b709a6e94b53a3e9e
yosefHesham <yousefmobdev@gmail.com>
2024-04-03 21:40:04 +0200

    Add Search Icon

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -1,24 +1,23 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { useState } from "react";
 
 const SearchModal = ({ handleSubmit }) => {
-    // const [inputValue, setInputValue] = useState('');
 
     return <section className="rounded border shadow-md  w-96 absolute p-8 top-10 flex bg-white gap-5">
 
         <p>Track Your Shippment</p>
-        <div className="flex border rounded-m">
-            <input type="text" onKeyDown={(event) => {
+        <div className="flex border rounded-xl">
+            <input type="text" placeholder="Shippment No" className="rounded-m px-5" onKeyDown={(event) => {
                 if (event.key == 'Enter') {
                     console.log(event.target.value)
                     handleSubmit(event.target.value)
                 }
             }}></input>
 
-            <div className="bg-red-500 w-4  h-5">
+            <div className="bg-red-500 w-12  h-full rounded-tr-md flex items-center justify-center rounded-br-md">
 
-                <FontAwesomeIcon />
-            </div>
+                <FontAwesomeIcon icon="fa-solid fa-search" color="white" />            </div>
         </div>
 
     </section>
